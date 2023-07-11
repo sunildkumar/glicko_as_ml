c8be3a64fe04664285557e652e71a0e21e5fb933
Sunil Kumar <sdkumar@hmc.edu>
2023-07-11 08:33:37 -0700

    commiting work from the weekend - simulation figures added

--- glicko_experiment2.py
+++ glicko_experiment2.py
@@ -72,6 +72,9 @@ def win_rate_model(player_skill: float, game_difficulty: float) -> float:
 
 
 def win_rate_lookup_table(players: list[Player], games: list[Game]) -> np.ndarray:
+    """
+    Precomputes the win rate of each player against each game. Returns a lookup table of the form lookup_table[game_index, player_index] = win_rate(player, game)
+    """
     true_win_probs = np.zeros((len(games), len(players)))
     for player_index, player in enumerate(players):
         for game_index, game in enumerate(games):
@@ -144,6 +147,90 @@ def create_true_model_figure(
     plt.close()
 
 
+def make_skill_rating_figure(players: list[Player], games: list[Game], iteration):
+    """
+    plots the skill/difficulty of each player/game against its glicko rating
+    """
+    player_ratings = [player.rating for player in players]
+    player_skills = [player.hidden_skill_mean for player in players]
+
+    game_ratings = [game.rating for game in games]
+    game_difficulties = [game.get_hidden_difficulty() for game in games]
+
+    players_2RD = [2 * player.rd for player in players]
+    games_2RD = [2 * game.rd for game in games]
+
+    plt.scatter(
+        player_ratings,
+        player_skills,
+    )
+    plt.scatter(game_ratings, game_difficulties)
+    errorbar_kwargs = dict(
+        fmt="o", capsize=5, markersize=6, markeredgewidth=1.5, elinewidth=1.5
+    )
+    plt.errorbar(
+        player_ratings,
+        player_skills,
+        xerr=players_2RD,
+        label="Players 95% confidence interval",
+        **errorbar_kwargs,
+    )
+    plt.errorbar(
+        game_ratings,
+        game_difficulties,
+        xerr=games_2RD,
+        label="Games 95% confidence interval",
+        **errorbar_kwargs,
+    )
+    plt.xlabel("Glicko Rating")
+    plt.ylabel("Hidden skill/difficulty")
+    plt.legend()
+    plt.title(f"Rounds: {iteration}")
+    plt.savefig(f"figures2/{iteration}.png")
+    plt.close()
+
+
+def predicted_win_rate_table(players: list[Player], games: list[Game]) -> np.ndarray:
+    """
+    given a list of players and games, returns a table of the form predicted_win_probs[game_index, player_index] = predicted_win_probability(player, game)
+    """
+
+    predicted_win_probs = np.zeros((len(games), len(players)))
+    for player_index, player in enumerate(players):
+        for game_index, game in enumerate(games):
+            predicted_win_probability = GlickoPlayer.expected_outcome(a=player, b=game)
+            predicted_win_probs[game_index, player_index] = predicted_win_probability
+
+    return predicted_win_probs
+
+
+def make_predicted_model_figure(players: list[Player], games: list[Game]):
+    predicted_table = predicted_win_rate_table(players, games)
+
+    colors = ["darkred", "red", "orange", "lime", "green"]
+    cmap = LinearSegmentedColormap.from_list("mycmap", colors)
+    norm = matplotlib.colors.Normalize(vmin=0.0, vmax=1.0)
+
+    plt.imshow(predicted_table, cmap=cmap, interpolation="nearest", norm=norm)
+    plt.xlabel("Players organized from worst to best")
+    plt.ylabel("Games organized from easiest to hardest")
+    plt.colorbar()
+    plt.title("Estimated probability a player wins game")
+    plt.savefig("figures2/predicted_win_probs.png")
+    plt.close()
+
+
+def mse_2d(arr1, arr2):
+    """
+    calculates the mean squared error between two 2d arrays elementwise
+    """
+    assert arr1.shape == arr2.shape, "Arrays must have the same shape"
+    flat_arr1 = arr1.flatten()
+    flat_arr2 = arr2.flatten()
+    mse = np.mean((flat_arr1 - flat_arr2) ** 2)
+    return mse
+
+
 def simulation(
     players: list[Player],
     games: list[Game],
@@ -153,15 +240,25 @@ def simulation(
     """
     Runs the simulation for num_iterations iterations. In each iteration, each player plays a random game
     """
-    for _ in tqdm(range(num_iterations)):
+    for iteration in tqdm(range(num_iterations)):
         # in each round each player plays a random game
         for player_index, player in enumerate(players):
             game_index = random.randint(0, len(games) - 1)
             game = games[game_index]
             play_game(player, game, player_index, game_index, lookup_table)
+        if iteration % (int(0.02 * num_iterations)) == 0:
+            make_skill_rating_figure(players, games, iteration)
+
+    # save final figure
+    make_skill_rating_figure(players, games, num_iterations)
 
 
-players, games = create_players_and_games(1000, 1000)
-lookup_table = win_rate_lookup_table(players, games)
-create_true_model_figure(players, games, lookup_table)
-simulation(players, games, 1000, lookup_table)
+players, games = create_players_and_games(num_players=1000, num_games=1000)
+lookup_table = win_rate_lookup_table(players=players, games=games)
+create_true_model_figure(players=players, games=games, true_win_probs=lookup_table)
+simulation(players=players, games=games, num_iterations=100, lookup_table=lookup_table)
+make_predicted_model_figure(players=players, games=games)
+
+predicted_table = predicted_win_rate_table(players, games)
+mse = mse_2d(lookup_table, predicted_table)
+print(f"Mean squared error between true and predicted win rates: {mse}")
